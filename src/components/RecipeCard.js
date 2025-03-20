a15406771a2b2d7e9a7fd90938b04f299399e8db
Christian Varela <dobleclick.mx@gmail.com>
2025-03-20 16:32:11 -0400

    Recipe card fixes

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -17,8 +17,6 @@ const RecipeCard = () => {
             }),
     )
 
-    console.log(23, recipes)
-
     return (
         <div className="container mx-auto px-4 py-8">
             <h1 className="text-2xl font-bold mb-6">Recipes</h1>
